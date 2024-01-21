63f6707046b0a506c42df274b362e55ebbb244e8
wesley <wesleyfaraujjo@gmail.com>
2024-01-20 23:01:02 -0300

    created the Raffle table

---
+++ Raffle/Models/Raffle.cs
@@ -0,0 +1,12 @@
+﻿namespace Raffle.Models
+{
+    public class Raffle
+    {
+        public int Id { get; set; }
+        public int prizeValue {  get; set; } 
+        public  DateTime dataRaffle {  get; set; }
+        public string result { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}

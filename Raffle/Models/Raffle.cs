864b0ca8b9a21a50d150a1277d924fe062b1e376
wesley <wesleyfaraujjo@gmail.com>
2024-01-20 23:02:58 -0300

    field changed to decimal

--- Raffle/Models/Raffle.cs
+++ Raffle/Models/Raffle.cs
@@ -3,7 +3,7 @@
     public class Raffle
     {
         public int Id { get; set; }
-        public int prizeValue {  get; set; } 
+        public decimal prizeValue {  get; set; } 
         public  DateTime dataRaffle {  get; set; }
         public string result { get; set; }
         public DateTime CreatedAt { get; set; }

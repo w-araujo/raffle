b004bfa5f5032f8e06bcc0b4349eee9a46455b06
wesley <wesleyfaraujjo@gmail.com>
2024-01-23 23:55:40 -0300

    added route to list the raffles

---
+++ Raffle/Controllers/Raffle1Controller.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Raffle.Database;
+using Raffle.Models;
+
+namespace Raffle.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class Raffle1Controller : ControllerBase
+    {
+        private readonly ILogger<BetController> _logger;
+        private readonly DbConnection _dbConnection;
+
+        public Raffle1Controller(ILogger<BetController> logger, DbConnection dbConnection)
+        {
+            _logger = logger;
+            _dbConnection = dbConnection;
+        }
+
+        [HttpGet(Name = "Raffle")]
+        public IActionResult GetRaffles()
+        {
+            try
+            {
+                var raffles = _dbConnection.Set<Raffle1>().ToList();
+                Console.WriteLine(_logger);
+                return Ok(raffles);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"Erro ao listar os sorteios: {ex.Message}");
+            }
+        }
+    }
+    }
